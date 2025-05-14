43fd5512dbef595ff1239846aff93e2e08e35008
luAlves04 <luan04marcos@gmail.com>
2025-05-13 21:05:55 -0300

    Start_page.py atualizada e quase completa

--- start_page.py
+++ start_page.py
@@ -1,17 +1,71 @@
-execucao = True
+#Bibliotecas
+import login
+import cadastro
 import keyboard
 
+#Variáveis
+execucao_um = True
+execucao_dois = True
+empresa = 'Empresa'
+cliente = 'Cliente'
 
-print('Bem vindo ao AllerGenie!\nSelecione uma das seguintes opções:\n1. Login \n2. Cadastro')
 
-while execucao == True:
-    if keyboard.read_key() == '1':
-        print('Realizando login')
-        execucao = False
-    elif keyboard.read_key() == '2':
-        print('Realizando cadastro')
-        execucao = False
-    else:
-        print('Inserção inválida.')
+
+#Definir se é empresa ou cliente
+while execucao_um == True:
+    print('Bem vindo ao AllerGenie!\nSelecione uma das seguintes opções:\n1. Empresa \n2. Cliente ')
     
+    tecla = keyboard.read_key() #Guarda a tecla pressionada
+
+    #Erro de inserção inválida
+    if tecla not in ['1', '2']:
+        print("\nErro: Inserção inválida\n")
+    else:
+        execucao_um = False
+    
+    #Printa o que foi escolhido inicialmente
+    if tecla == '1':
+        print("\nEscolhido: ", empresa)
+    elif tecla == '2':
+        print("\nEscolhido: ", cliente)
+
+
+
+#Escolher entre login e cadastro ou cancelar
+while execucao_dois == True:
+    tecla_log_cad = int(input('Bem vindo ao AllerGenie!\nSelecione uma das seguintes opções:\n1. Login \n2. Cadastro \n3. Cancelar : ', ))
+
+    #Caso escolheu login
+    if tecla_log_cad == 1:
+        print('Realizando login')
+        #Se a primeira escolha foi empresa
+        if tecla == '1':
+            print("Realizando o login de empresa")
+        #Se a primeira escolha foi cliente
+        elif tecla == '2':
+            print("Realizando o login de cliente")
+
+        execucao_dois = False
+
+    
+    #Caso escolheu cadastro
+    elif tecla_log_cad == 2:
+        print('Realizando cadastro')
+        #Se a primeira escolha foi empresa
+        if tecla == '1':
+            print("Realizando o cadastro de empresa")
+        #Se a primeira escolha foi cliente
+        elif tecla == '2':
+            print("IRealizando o cadastro de cliente")
+
+        execucao_dois = False
+    elif tecla_log_cad == 3:
+        print("Cancelando. . .")
+        execucao_dois = False
+    #Erro
+    else:
+        print('\nErro: Inserção inválida. Tente novamente\n')
+
+
 #__name__ == '__main__'
+
